4647c21329f50326e85235570e1a2368163506ef
Jefferson Moreno <prowlerurbanhc@gmail.com>
2021-09-07 12:56:41 -0300

    Order, Instant, ISO 8601 PART 2

---
+++ src/main/java/br/com/portaljmti/services/OrderService.java
@@ -0,0 +1,27 @@
+package br.com.portaljmti.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.portaljmti.entities.Order;
+import br.com.portaljmti.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+
+	public Order findById(Long id) {
+
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
