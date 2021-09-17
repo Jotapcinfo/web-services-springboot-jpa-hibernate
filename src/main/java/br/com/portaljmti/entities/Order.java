b7669f460a4aebe07ed14818eaa7473373ab2ed4
Jefferson Moreno <prowlerurbanhc@gmail.com>
2021-09-16 22:56:33 -0300

    Subtotal & Total methods

--- src/main/java/br/com/portaljmti/entities/Order.java
+++ src/main/java/br/com/portaljmti/entities/Order.java
@@ -40,7 +40,7 @@ public class Order implements Serializable {
 
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
-	
+
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 
@@ -89,6 +89,14 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -96,7 +104,7 @@ public class Order implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-    
+
 	public Payment getPayment() {
 		return payment;
 	}
